656060594ce00916c611288871dacc6e70982f03
meVarensev <vitalik.unit@gmail.com>
2023-10-16 19:11:32 +0300

    feat: quick start

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <h1>App</h1>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <p>home view page</p>
+  </main>
+</template>
