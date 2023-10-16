85aebe017a7ec51e1bec5b976fd926173b18e49b
meVarensev <vitalik.unit@gmail.com>
2023-10-16 19:39:41 +0300

    feat: quick start

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-<<<<<<< HEAD
 import UiContainer from "@/components/ui-container.vue";
 import MainContent from "@/components/main-content/main-content.vue";
 import UiHeader from "@/components/ui-header/ui-header.vue";
 import UiFooter from "@/components/footer/ui-footer.vue";
-=======
 
->>>>>>> 656060594ce00916c611288871dacc6e70982f03
 </script>
 
 <template>
 
-<<<<<<< HEAD
   <UiContainer>
     <template #header>
       <UiHeader/>
@@ -25,10 +21,6 @@ import UiFooter from "@/components/footer/ui-footer.vue";
       <UiFooter/>
     </template>
   </UiContainer>
-=======
-  <h1>App</h1>
-
->>>>>>> 656060594ce00916c611288871dacc6e70982f03
 </template>
 
 <style scoped>
